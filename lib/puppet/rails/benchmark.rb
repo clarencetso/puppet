e08ef0f3924636b11082d28bf13461177a8bdc10
Luke Kanies <luke@madstop.com>
2009-04-08 18:47:26 -0500

    Adding some more fine-grained benchmarks to Rails support
    Signed-off-by: Luke Kanies <luke@madstop.com>

--- lib/puppet/rails/benchmark.rb
+++ lib/puppet/rails/benchmark.rb
@@ -2,12 +2,39 @@ require 'benchmark'
 module Puppet::Rails::Benchmark
     def railsmark(message)
         seconds = Benchmark.realtime { yield }
-        Puppet.debug(message + " %0.2f seconds" % seconds)
+        Puppet.debug(message + " in %0.2f seconds" % seconds)
     end
 
     def sometimes_benchmark(message)
         yield and return unless Puppet::Rails::TIME_DEBUG
 
         railsmark(message) { yield }
+    end
+
+    # Collect partial benchmarks to be logged when they're
+    # all done.
+    #   These are always low-level debugging so we only
+    # print them if time_debug is enabled.
+    def accumulate_benchmark(message, label)
+        yield and return unless Puppet::Rails::TIME_DEBUG
+
+        $accumulated_benchmarks ||= {}
+        $accumulated_benchmarks[message] ||= Hash.new(0)
+        $accumulated_benchmarks[message][label] += Benchmark.realtime { yield }
+    end
+
+    # Log the accumulated marks.
+    def log_accumulated_marks(message)
+        return unless Puppet::Rails::TIME_DEBUG
+
+        if $accumulated_benchmarks.empty? or $accumulated_benchmarks[message].empty?
+            Puppet.debug(message + " in %0.2f seconds" % 0)
+            return
+        end
+
+        $accumulated_benchmarks[message].each do |label, value|
+            Puppet.debug(message + ("(%s)" % label) + (" in %0.2f seconds" % value))
+        end
+        $accumulated_benchmarks.delete(message)
     end
 end
