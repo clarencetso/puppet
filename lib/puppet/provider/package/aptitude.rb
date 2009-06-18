c5c9b06f9d88f70cf6641255e4e93d9d77b3556a
Clarence Tso <clarence@invitemedia.com>
2009-06-18 13:18:05 -0400

    Aptitude provider makes a dry run to ensure version exists, and also check for unmet dependencies and 404's

--- lib/puppet/provider/package/aptitude.rb
+++ lib/puppet/provider/package/aptitude.rb
@@ -14,13 +14,39 @@ Puppet::Type.type(:package).provide :aptitude, :parent => :apt, :source => :dpkg
         if args.include?("-q")
             args.delete("-q")
         end
+
+        #Do a dry run to make sure the version exists - Otherwise, aptitude ends up
+        #installing the latest version if it can't find the version
+        #Also check for any unmet dependencies from Aptitude
+        if args.include?(:install)
+            args_dry_run = Array.new(args)
+            args_dry_run.push("-s")
+            output = aptitude(*args_dry_run)
+            if output =~ /Unable to find a version/
+                raise Puppet::Error.new(
+                    "Could not find specified version for package %s" % self.name
+                )
+            elsif output =~ /unmet dependencies/
+                raise Puppet::Error.new(
+                    "Package %s has unmet dependencies" % self.name
+                )
+            end
+        end
+
         output = aptitude(*args)
 
         # Yay, stupid aptitude doesn't throw an error when the package is missing.
-        if args.include?(:install) and output =~ /Couldn't find any package/
-            raise Puppet::Error.new(
-                "Could not find package %s" % self.name
-            )
+        #Also check for 404 Not Founds from the repositories used
+        if args.include?(:install)
+            if output =~ /Couldn't find any package/
+                raise Puppet::Error.new(
+                    "Could not find package %s" % self.name
+                )
+            elsif output =~ /404 Not Found/
+                raise Puppet::Error.new(
+                    "Repository returned a 404 Not Found for package %s" % self.name
+                )
+            end
         end
     end
 
